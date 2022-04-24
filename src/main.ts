ffc47221df6a06f6200f8a74c449ed83f030dda3
luzhiyu <lushuke5@gmail.com>
2022-04-24 14:41:36 +0800

    test3

--- src/main.ts
+++ src/main.ts
@@ -29,7 +29,7 @@ window.onload = function () {
 if (document.documentElement.clientWidth > 500) {
     window.alert('请使用手机打开页面保证预览效果');
     const img = document.createElement('img');
-    img.src = 'public/erweima.png';
+    img.src = '../public/erweima.png';
     img.style.position = 'fixed';
     img.style.left = '50%';
     img.style.top = '50%';
